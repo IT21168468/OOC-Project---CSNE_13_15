e539ae9f655b2a4325d50d20a32b57c6413b4829
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-18 04:12:35 +0530

    Owner.h

---
+++ Owner.h
@@ -0,0 +1,31 @@
+// IT21168468
+
+#include "Person.h"
+#include "Pet.h"
+#include "Payment.h"
+#include "Item.h"
+#include "DayCare.h"
+#include "Appointment.h"
+#include <cstring>
+
+using namespace std;
+
+class Owner : public Person {
+  protected:
+    string OwnerId;
+    Pet *pet[size];
+    Payment *pay[size];
+    Item *itm[size];
+    Appointment *app[size];
+    DayCare *dcr[size];
+  public:
+  	Owner();
+  	Owner(int pet1, int pay1, int itm1, string ownerid, string name, string email, string contactno);
+	void Display();
+	void DisplayPet();
+	void DisplayPayment();
+	void DisplayOwner();
+	void AddItem();
+	void AddAppointment();
+    ~Owner();
+};
