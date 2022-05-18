a0cea381ad090dad5cd3c331cae9bd2f3e9caec1
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-19 05:06:15 +0530

    Staff.h

--- Staff.h
+++ Staff.h
@@ -6,15 +6,16 @@
 #include "DayCare.h"
 #include "Item.h"
 #include <cstring>
+#define SIZE 1
 
 using namespace std;
 
 class Staff : public Person {
   protected:
     string StaffId;
-    Item *itm[size];
-    DayCare *dcr[size];
-    Doctor *doc[size];
+    Item *itm[SIZE];
+    DayCare *dcr[SIZE];
+    Doctor *doc[SIZE];
   public:
     Staff();
     Staff(string staffid, string name, string email, string contactno);
