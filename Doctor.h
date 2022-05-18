f8da57722ee12751b6a3358e00a6a012c2bd9ddc
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-19 05:01:56 +0530

    Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -3,13 +3,14 @@
 #include "Person.h"
 #include "Appointment.h"
 #include <cstring>
+#define SIZE 1
 
 using namespace std;
 
 class Doctor : public Person {
   protected:
     string DoctorId;
-    Appointment *app[size];
+    Appointment *app[SIZE];
   public:
     Doctor();
     Doctor(string doctorid, string name, string email, string contactno, int app1);
