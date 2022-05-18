d9897c7f73662d77cc4a61b0320590cd258997d4
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-19 05:04:55 +0530

    Owner.h

--- Owner.h
+++ Owner.h
@@ -7,17 +7,18 @@
 #include "DayCare.h"
 #include "Appointment.h"
 #include <cstring>
+#define SIZE 1
 
 using namespace std;
 
 class Owner : public Person {
   protected:
     string OwnerId;
-    Pet *pet[size];
-    Payment *pay[size];
-    Item *itm[size];
-    Appointment *app[size];
-    DayCare *dcr[size];
+    Pet *pet[SIZE];
+    Payment *pay[SIZE];
+    Item *itm[SIZE];
+    Appointment *app[SIZE];
+    DayCare *dcr[SIZE];
   public:
   	Owner();
   	Owner(int pet1, int pay1, string ownerid, string name, string email, string contactno);
