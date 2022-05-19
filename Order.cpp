8c86e9237132a5f4c29e375ab9657cc4d451342c
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-19 17:43:54 +0530

    Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -10,19 +10,19 @@
 using namespace std;
 
 Order :: Order() {
-	      OrderNo = "";
-	      ShippingMethod = "";
-        itm[1] = new Item(61);
+	OrderNo = "";
+	ShippingMethod = "";
+	itm[1] = new Item(61);
 }
 
 Order :: Order(int orderno, char shippingmethod, int itm1) {
-	      OrderNo = orderno;
-	      ShippingMethod = shippingmethod;
+	OrderNo = orderno;
+	ShippingMethod = shippingmethod;
         itm[1] = new Item(itm1);
 }
 
 void Order :: Display() {
-  cout << "This is Order Class >> Derived from Person\n\n";
+	cout << "This is Order Class >> Derived from Person\n\n";
 }
 
 void Order :: DisplayOrder() {
